0c1ddce5970b5745de39b65fea85c9a0a90faa18
elshadday45 <118614225+elshadday45@users.noreply.github.com>
2024-11-12 18:31:15 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ onload = () => {
   const c = setTimeout(() => {
     document.body.classList.remove("not-loaded");
 
-    const titles = ('Happy Birthday Anwen Lo, Tuhan Mengasihimu').split('')
+    const titles = ('Happy Birthday Anwen Lo').split('')
     const titleElement = document.getElementById('title');
     let index = 0;
 
